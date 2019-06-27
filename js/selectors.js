656ca4010f4ff4618e9717a77c29874378fed362
Nicolas Theoret <nicolas.o.theoret@gmail.com>
2019-06-27 15:16:44 +0000

    Done.

--- js/selectors.js
+++ js/selectors.js
@@ -1,1 +1,25 @@
 // declare your functions here...
+function paragraphSelector() {
+  var pValue = $('p')
+  return pValue
+}
+
+function lastImageSelector() {
+  var imgLast = $('img:last')
+  return imgLast
+}
+
+function ninjaBabySelector() {
+  var ninja = $('#baby-ninja')
+  return ninja  
+}
+
+function divSelector() {
+  var diver = $('.pics')
+  return diver
+}
+
+function firstListItem() {
+  var picList = $('ul > :first-child')
+  return picList
+}
